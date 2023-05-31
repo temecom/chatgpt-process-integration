117b623f2ada7f71a94c6a23d868e63f61226f99
Christopher Smith <csmith@temecom.net>
2023-05-31 12:26:28 -0400

    attadhed to upstream

--- brainstorm/brainstorm-service/src/main/java/com/oopscope/brainstorm/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oopscope.brainstorm.repository;
-
-import com.oopscope.brainstorm.model.User;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import java.util.UUID;
-
-public interface UserRepository extends ReactiveCrudRepository<User, UUID> {
-}
